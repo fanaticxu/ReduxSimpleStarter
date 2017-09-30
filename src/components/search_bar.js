e29e5740f7a3f61790ec24e0fc27702dadb3fab1
Frank <xuzy213@gmail.com>
2017-10-01 00:25:20 +1300

    search term by pressing enter

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,17 +13,23 @@ class SearchBar extends Component {
             <div className='search-bar'>
                 <input
 
-                    value = {this.state.term}
-                    onChange={(event) => this.onInputChange(event.target.value)} />
+                    //value = {this.state.term}
+                    onChange={(event) => this.setState({term:event.target.value})}
+                    onKeyPress={ (event) => {
+                        if(event.charCode === 13) {
+                            this.props.onSearchTermChange(this.state.term);
+                            }
+                        }
+                    } />
             </div>
         );
             
     }
 
-    onInputChange(term) {
-        this.setState({term});
-        this.props.onSearchTermChange(term);
-    }
+    // onInputChange(term) {
+    //     this.setState({term});
+    //     this.props.onSearchTermChange(term);
+    // }
 }
 
 export default SearchBar;
