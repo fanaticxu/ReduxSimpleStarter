830c00ba1295fc2bbb931957bd55186fecadae0f
Frank <xuzy213@gmail.com>
2017-09-30 22:12:03 +1300

    try to print video title and img from a fix search using youtube api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDSIJv5O8LhPPM1to5wQo6o6uoVVp5qFDY';
 
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>;
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: []};
+
+        YTSearch({key: API_KEY, term: '爸爸去哪儿'}, (videos) => {
+            this.setState({ videos });
+            // this.setState({ videos: videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+            );
+    }
 }
+
 
 // Take this component's generated HTML and put it on the page(in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
